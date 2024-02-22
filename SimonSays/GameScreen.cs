9b3b526329cd1c20c6bf976f2ceed9aadd9d9f6b
Koen Gerber <koengerb477@outlook.com>
2024-02-21 22:32:28 -0500

    Pretty much done, submitting in case the due date is still tonight.

--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -9,58 +9,316 @@ using System.Windows.Forms;
 using System.Media;
 using System.Drawing.Drawing2D;
 using System.Threading;
+using System.Security.Cryptography;
+using System.Xml.Schema;
+using System.Drawing.Drawing2D;
 
 namespace SimonSays
 {
     public partial class GameScreen : UserControl
     {
-        //TODO: create an int guess variable to track what part of the pattern the user is at
+        int arrayIndex;
+        bool computerTurn = true;
+        bool playerTurn = false;
+        int counter = 0;
 
+        //TODO: create an int guess variable to track what part of the pattern the user is at
+        int guess;
+
+        const int DELAY_TIME = 50;
+
+        //arrays
+        Color[] secondColorArray = { Color.LawnGreen, Color.Red, Color.Blue, Color.Yellow };
+        Color[] firstColorArray = { Color.ForestGreen, Color.DarkRed, Color.DarkBlue, Color.Goldenrod };
+        SoundPlayer[] soundArray = { new SoundPlayer(Properties.Resources.green), new SoundPlayer(Properties.Resources.red), new SoundPlayer(Properties.Resources.blue), new SoundPlayer(Properties.Resources.yellow), new SoundPlayer(Properties.Resources.mistake) };
+        Button[] buttonArray = new Button[4];
+        int[] blinkTimer = { 0, 0, 0, 0 };
+
+        //star movement lists
+        List<PointF> starPoints = new List<PointF>();
+        List<int> starXSpeed = new List<int>();
+        List<int> starYSpeed = new List<int>();
 
         public GameScreen()
         {
             InitializeComponent();
+
+            buttonArray[0] = greenButton;
+            buttonArray[1] = redButton;
+            buttonArray[2] = blueButton;
+            buttonArray[3] = yellowButton;
         }
 
         private void GameScreen_Load(object sender, EventArgs e)
         {
+            //button regions for arc shape
+            GraphicsPath buttonPath = new GraphicsPath();
+            buttonPath.AddArc(5, 5, 200, 200, 180, 90);
+            buttonPath.AddArc(75, 75, 62, 62, 270, -90);
+
+            for (int i = 0; i < buttonArray.Length; i++)
+            {
+                buttonArray[i].Region = new Region(buttonPath);
+
+                Matrix transformMatrix = new Matrix();
+                transformMatrix.RotateAt(90, new PointF(55, 55));
+                buttonPath.Transform(transformMatrix);
+            }
+
             //TODO: clear the pattern list from form1
+            Form1.pattern.Clear();
             //TODO: refresh
+            Refresh();
             //TODO: pause for a bit
+            Thread.Sleep(DELAY_TIME);
             //TODO: run ComputerTurn()
+            ComputerTurn();
         }
 
+        //get new values for pattern
         private void ComputerTurn()
         {
-            //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list. Each number represents a button. For example, 0 may be green, 1 may be blue, etc.
 
-            //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            //TODO: get rand num between 0 and 4  (0, 1, 2, 3) and add to pattern list. Each number represents a button. For example, 0 may be green, 1 may be blue, etc.
+            Random randGen = new Random();
+            int randNum = randGen.Next(0, 4);
+            Form1.pattern.Add(randNum);
 
             //TODO: set guess value back to 0
-        }
-
-        //TODO: create one of these event methods for each button
-        private void greenButton_Click(object sender, EventArgs e)
-        {
-            //TODO: is the value in the pattern list at index [guess] equal to a green?
-                // change button color
-                // play sound
-                // refresh
-                // pause
-                // set button colour back to original
-                // add one to the guess variable
-             
-            //TODO:check to see if we are at the end of the pattern, (guess is the same as pattern list count).
-                // call ComputerTurn() method
-                // else call GameOver method
+            guess = 0;
         }
 
         public void GameOver()
         {
             //TODO: Play a game over sound
-
+            soundArray[4].Play();
             //TODO: close this screen and open the GameOverScreen
+            Form1.ChangeScreen(this, new GameOverScreen());
+        }
 
+        public void ClickEvent(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            //find which button was clicked and store it
+            for (int i = 0; i < buttonArray.Length; i++)
+            {
+                if (buttonArray[i] == button)
+                {
+                    arrayIndex = i;
+                }
+            }
+
+            //if button that flashed is the button that was pressed
+            if (Form1.pattern[guess] == arrayIndex)
+            {
+                //blink the button and play a sound
+                soundArray[arrayIndex].Play();
+                buttonArray[arrayIndex].BackColor = secondColorArray[arrayIndex];
+
+                guess++;
+            }
+            //else lose
+            else
+            {
+                GameOver();
+            }
+        }
+
+        private void starTimer_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            //make new stars
+            int randNum = random.Next(1, 101);
+            if (randNum > 99)
+            {
+                int y = random.Next(-this.Height, 0);
+                int x = random.Next(this.Width, this.Width * 2);
+
+                Point star = new Point(x, y);
+                starPoints.Add(star);
+                starXSpeed.Add(random.Next(-10, -1));
+                starYSpeed.Add(random.Next(1, 10));
+            }
+
+            //move stars
+            for (int i = 0; i < starPoints.Count; i++)
+            {
+                float x = starPoints[i].X + starXSpeed[i];
+                float y = starPoints[i].Y + starYSpeed[i];
+                starPoints[i] = new PointF(x, y);
+            }
+
+            //delete stars
+            for (int i = 0; i < starPoints.Count; i++)
+            {
+                if ((starPoints[i].X < -30))
+                {
+                    starPoints.RemoveAt(i);
+                    starXSpeed.RemoveAt(i);
+                    starYSpeed.RemoveAt(i);
+                }
+            }
+
+            //player turn
+            if (playerTurn == true)
+            {
+                for (int i = 0; i < blinkTimer.Length; i++)
+                {
+                    //if button is lit up increase its timer
+                    if (buttonArray[i].BackColor == secondColorArray[i])
+                    {
+                        blinkTimer[i]++;
+                    }
+
+                    //if buttons timer is above limit change it back to original colour
+                    if (blinkTimer[i] >= DELAY_TIME)
+                    {
+                        buttonArray[i].BackColor = firstColorArray[i];
+                        blinkTimer[i] = 0;
+                    }
+
+                    //if blink timer is reset and the guess number and pattern size are equal go to computer turn
+                    if (guess == Form1.pattern.Count && blinkTimer[i] == 0)
+                    {
+                        //blinking lights to show round over
+                        for (int g = 0; g < 4; g++)
+                        {
+                            for (int f = 0; f < buttonArray.Length; f++)
+                            {
+                                buttonArray[f].BackColor = secondColorArray[f];
+                            }
+                            this.Refresh();
+                            Thread.Sleep(300);
+                            for (int f = 0; f < buttonArray.Length; f++)
+                            {
+                                buttonArray[f].BackColor = firstColorArray[f];
+                            }
+                            this.Refresh();
+                            Thread.Sleep(300);
+                        }
+
+                        //reset and change to computer turn
+                        ComputerTurn();
+                        counter = 0;
+
+                        computerTurn = true;
+                        playerTurn = false;
+                    }
+                }
+            }
+
+            //computer turn
+            if (computerTurn == true)
+            {
+                //go one by one through the pattern lighting up buttons
+
+                arrayIndex = Form1.pattern[counter];
+
+                //play sound only once
+                if (blinkTimer[arrayIndex] == 0)
+                {
+                    soundArray[arrayIndex].Play(); ;
+                }
+                //wait while light is on
+                if (blinkTimer[arrayIndex] < DELAY_TIME)
+                {
+                    buttonArray[arrayIndex].BackColor = secondColorArray[arrayIndex];
+                    blinkTimer[arrayIndex]++;
+                }
+                //wait while light is off
+                else if (blinkTimer[arrayIndex] < DELAY_TIME * 2)
+                {
+                    buttonArray[arrayIndex].BackColor = firstColorArray[arrayIndex];
+                    blinkTimer[arrayIndex]++;
+                }
+                //reset timer and move on to next part of the pattern
+                else
+                {
+                    blinkTimer[arrayIndex] = 0;
+                    counter++;
+                }
+
+                //if at the end of the pattern switch to player turn
+                if (counter == Form1.pattern.Count)
+                {
+                    computerTurn = false;
+                    playerTurn = true;
+                }
+            }
+
+            this.Refresh();
+        }
+
+        //star method from draw star assignment
+        public void DrawOrFillStar(object drawTool, float x, float y, float pixels)
+        {
+            PointF point1 = new PointF(0, 80);
+            PointF point2 = new PointF(85, 80);
+            PointF point3 = new PointF(110, 0);
+            PointF point4 = new PointF(140, 80);
+            PointF point5 = new PointF(225, 80);
+            PointF point6 = new PointF(160, 130);
+            PointF point7 = new PointF(180, 210);
+            PointF point8 = new PointF(110, 160);
+            PointF point9 = new PointF(40, 210);
+            PointF point10 = new PointF(70, 130);
+            PointF[] points = new PointF[] { point1, point2, point3, point4, point5, point6, point7, point8, point9, point10 };
+
+            if (drawTool is Pen)
+            {
+                //draw
+                Pen starPen;
+                starPen = (Pen)drawTool;
+
+                Graphics g = this.CreateGraphics();
+
+                float scaleFactor = pixels / 225;
+
+                for (int i = 0; i < points.Length; i++)
+                {
+                    points[i].X *= scaleFactor;
+                    points[i].Y *= scaleFactor;
+
+                    points[i].X += x;
+                    points[i].Y += y;
+                }
+
+                g.DrawPolygon(starPen, points);
+            }
+            else
+            {
+                //fill
+                SolidBrush starBrush;
+                starBrush = (SolidBrush)drawTool;
+
+                Graphics g = this.CreateGraphics();
+
+                float scaleFactor = pixels / 225;
+
+                for (int i = 0; i < points.Length; i++)
+                {
+                    points[i].X *= scaleFactor;
+                    points[i].Y *= scaleFactor;
+
+                    points[i].X += x;
+                    points[i].Y += y;
+                }
+
+                g.FillPolygon(starBrush, points);
+            }
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush starBrush = new SolidBrush(Color.Yellow);
+
+            //draw stars
+            for (int i = 0; i < starPoints.Count; i++)
+            {
+                DrawOrFillStar(starBrush, starPoints[i].X, starPoints[i].Y, 20);
+            }
         }
     }
 }
--- SimonSays/MenuScreen.cs
+++ SimonSays/MenuScreen.cs
@@ -6,25 +6,143 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Diagnostics;
 
 namespace SimonSays
 {
     public partial class MenuScreen : UserControl
     {
+        //variables for animation
+        PointF origin = new PointF(100, 100);
+        float leftX;
+        float rightX;
+        float topY;
+        float bottomY;
+        float size;
+        float startAngle;
+        float spinSpeed = 3;
+        int colour = 0;
+
+        Pen greenPen = new Pen(Color.DarkGreen, 100);
+        Pen redPen = new Pen(Color.DarkRed, 100);
+        Pen bluePen = new Pen(Color.DarkBlue, 100);
+        Pen yellowPen = new Pen(Color.Goldenrod, 100);
+
+        Pen[] pens = new Pen[4];
+
         public MenuScreen()
         {
             InitializeComponent();
+            size = 200;
+            startAngle = 1;
+
+            pens[0] = greenPen;
+            pens[1] = redPen;
+            pens[2] = bluePen;
+            pens[3] = yellowPen;
         }
 
+        //button press runs animation then sends to gamescreen
         private void newButton_Click(object sender, EventArgs e)
         {
             //TODO: remove this screen and start the GameScreen
+            Animation(360, 125, new Point(72, 68), 70, false, true);
+
+            Form1.ChangeScreen(this, new GameScreen());
         }
 
-
+        //runs animation then exits
         private void exitButton_Click(object sender, EventArgs e)
         {
             //TODO: end the application
+            Animation(100000, 300, new Point(-5, -5), 200, true, true);
+
+            Application.Exit();
+        }
+
+        private void MenuScreen_Paint(object sender, PaintEventArgs e)
+        {
+            leftX = origin.X + 5;
+            rightX = origin.X + 25;
+            topY = origin.Y + 5;
+            bottomY = origin.Y + 25;
+
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(colour,0,0,0)), new Rectangle(0,0,this.Width,this.Height));
+
+            e.Graphics.DrawArc(greenPen, leftX, topY, size, size, startAngle + 178, 94);
+            e.Graphics.DrawArc(redPen, rightX, topY, size, size, startAngle + 268, 94);
+            e.Graphics.DrawArc(bluePen, rightX, bottomY, size, size, startAngle + 358, 94);
+            e.Graphics.DrawArc(yellowPen, leftX, bottomY, size, size, startAngle + 88, 94);
+        }
+
+        public void Animation(int endAngle, int endSize, Point endPoint, int endPenWidth, bool exit, bool colourChange)
+        {
+            //continue movement until it spins enough
+            while (startAngle != endAngle && startAngle < 10000)
+            {
+                //rotation
+                if (startAngle < endAngle)
+                {
+                    startAngle += spinSpeed;
+                }
+                else
+                {
+                    startAngle--;
+                }
+                //size
+                if (size > endSize)
+                {
+                    size--;
+                }
+                else if (size < endSize)
+                {
+                    size++;
+                }
+                //location x
+                if (origin.X >= endPoint.X)
+                {
+                    origin.X--;
+                }
+                else
+                {
+                    origin.X++;
+                }
+                //location y
+                if(origin.Y >= endPoint.Y)
+                {
+                    origin.Y--;
+                }
+                else
+                {
+                    origin.Y++;
+                }
+                //pen size
+                for(int i = 0; i < 4; i++)
+                {
+                    if (pens[i].Width > endPenWidth)
+                    {
+                        pens[i].Width--;
+                    }
+                    else if (pens[i].Width <  endPenWidth)
+                    {
+                        pens[i].Width++;
+                    }
+                }
+                //background colour
+                if(colour < 254 && colourChange)
+                {
+                    colour+=2;
+                }
+                //for exit animation accelerate
+                if (exit)
+                {
+                    spinSpeed += 0.5f;
+                }
+                this.Refresh();
+                Thread.Sleep(20);
+            }
         }
     }
 }
